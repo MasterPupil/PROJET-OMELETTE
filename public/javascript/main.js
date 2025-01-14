48bd06883bc19452a2348a14aaaa52ef9a4e3f68
MasterPupil <ahmedkamil12@gmail.com>
2025-01-14 11:33:29 +0100

    Initial commit : Ajout du fichier README.md

---
+++ public/javascript/main.js
@@ -0,0 +1,65 @@
+//exe 2
+//let reponse = prompt("Entrez quelque chose :");
+//alert("Vous avez entré : " + reponse);
+
+// exe3
+//let prenom = prompt("Quel est votre prénom ?");
+//alert("Bonjour " + prenom);
+
+// exe3.1
+//let age = prompt("Quel âge avez-vous ?");
+//alert("Tu as " + age + " ans");
+
+// exe4
+//let prenom = prompt("Quel est votre prénom ?");
+//let nom = prompt("Quel est votre nom de famille ?");
+// pour stocker
+//let identite = prenom + " " + nom;
+//console.log("Votre nom complet est : " + identite);
+
+// exe 5
+//let prenom = prompt("Quel est votre prénom ?");
+//let nom = prompt("Quel est votre nom de famille ?");
+
+//let nomEnMajuscule = nom.toUpperCase();
+// pour stocker
+//let identite = nomEnMajuscule + " " + prenom;
+//alert("Bonjour " + identite);
+
+//exe1
+/*let a = 1;
+let b = 2;
+let c = 3;
+
+let temp = a;
+a = b;
+b = c;
+c = temp;
+
+console.log(a, b, c); */
+
+//exe2
+/*
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+let temp = a; 
+a = d;
+d = temp;     // `d` prend la valeur initiale de `a` (1)
+
+temp = b;     // Stocke la valeur de `b`
+b = c;        // `b` prend la valeur de `c` (3)
+c = temp;     // `c` prend la valeur initiale de `b` (2)
+
+console.log(a, b, c, d); // Affiche : 4, 3, 2, 1 */
+
+
+
+
+
+
+
+
+
