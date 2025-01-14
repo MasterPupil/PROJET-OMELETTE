761d01cf2af76835dbc04a1d6bce4b33d8eda5dd
MasterPupil <ahmedkamil12@gmail.com>
2025-01-14 20:50:00 +0100

    2

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,7 +13,7 @@ let personnage = {
 
 let maison = {
     nom: "maison",
-    personnes: [] // Personnes dans la maison
+    personnes: [] //personnes dans la maison
 };
 
 let epicerie = {
@@ -27,6 +27,28 @@ console.log(`Début : ${personnage.nom} est à la ${personnage.lieu}.`);
 personnage.seDeplacer(epicerie);
 console.log(`${personnage.nom} est maintenant à la ${personnage.lieu}.`);
 
+// listes disponibles à l'épicerie
+epicerie.ingredients = [
+    { nom: "oignon", etat: "entier", prix: 2 },
+    { nom: "oeuf", etat: "entier", prix: 3 },
+    { nom: "fromage", etat: "entier", prix: 4 },
+    { nom: "épices", etat: "entier", prix: 1 }
+];
+
+// pour la prise de panier
+personnage.prendrePanier = function () {
+    if (epicerie.paniers.length > 0) {
+        let panier = epicerie.paniers.pop(); // pourretirer au panier 
+        this.mainDroite.push(panier); // pour ajouter au panier
+        console.log(`${this.nom} a pris un panier.`);
+    } else {
+        console.log(`Il n'y a plus de panier disponible à l'épicerie.`);
+    }
+};
+
+personnage.seDeplacer(epicerie);
+personnage.prendrePanier();
+console.log(`${personnage.nom} a dans sa main droite :`, personnage.mainDroite);
 
 
 
