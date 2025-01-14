aa5f2443d04f2c2306c00b50dcd336c943e8b257
MasterPupil <ahmedkamil12@gmail.com>
2025-01-14 11:48:46 +0100

    Début Début

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,63 +1,31 @@
-//exe 2
-//let reponse = prompt("Entrez quelque chose :");
-//alert("Vous avez entré : " + reponse);
-
-// exe3
-//let prenom = prompt("Quel est votre prénom ?");
-//alert("Bonjour " + prenom);
-
-// exe3.1
-//let age = prompt("Quel âge avez-vous ?");
-//alert("Tu as " + age + " ans");
-
-// exe4
-//let prenom = prompt("Quel est votre prénom ?");
-//let nom = prompt("Quel est votre nom de famille ?");
-// pour stocker
-//let identite = prenom + " " + nom;
-//console.log("Votre nom complet est : " + identite);
-
-// exe 5
-//let prenom = prompt("Quel est votre prénom ?");
-//let nom = prompt("Quel est votre nom de famille ?");
-
-//let nomEnMajuscule = nom.toUpperCase();
-// pour stocker
-//let identite = nomEnMajuscule + " " + prenom;
-//alert("Bonjour " + identite);
-
-//exe1
-/*let a = 1;
-let b = 2;
-let c = 3;
-
-let temp = a;
-a = b;
-b = c;
-c = temp;
-
-console.log(a, b, c); */
-
-//exe2
-/*
-let a = 1;
-let b = 2;
-let c = 3;
-let d = 4;
-
-let temp = a; 
-a = d;
-d = temp;     // `d` prend la valeur initiale de `a` (1)
-
-temp = b;     // Stocke la valeur de `b`
-b = c;        // `b` prend la valeur de `c` (3)
-c = temp;     // `c` prend la valeur initiale de `b` (2)
-
-console.log(a, b, c, d); // Affiche : 4, 3, 2, 1 */
+let personnage = {
+    nom: "Ahmed",
+    lieu: "maison",
+    argent: 50,
+    mainDroite: [], // rableau vide pour le panier
+    mainGauche: [], //sert à rien pour l'instant 
+    seDeplacer(lieu) {
+        this.lieu = lieu.nom; // pour le changement de lieu du personnage
+        console.log(`${this.nom} se déplace vers ${lieu.nom}.`);
+    }
+};
 
 
+let maison = {
+    nom: "maison",
+    personnes: [] // Personnes dans la maison
+};
 
+let epicerie = {
+    nom: "épicerie",
+    personnes: [], //personnes dans l'épicerie
+    paniers: [{ type: "panier", contenu: [] }, { type: "panier", contenu: [] }]
+};
 
+// Testtest tessst
+console.log(`Début : ${personnage.nom} est à la ${personnage.lieu}.`);
+personnage.seDeplacer(epicerie);
+console.log(`${personnage.nom} est maintenant à la ${personnage.lieu}.`);
 
 
 
